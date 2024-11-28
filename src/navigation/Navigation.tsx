0d47cfefb3797b37a7fe5c8fe32be6e888cc8aaf
Raul99GC <raulc99g@gmail.com>
2024-11-27 21:20:55 -0600

    Set up Navigation component with routes and 404 handling

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,4 +1,5 @@
 import SiteHeader from '@/components/SiteHeader'
+import Error404 from '@/screen/Error404'
 import Home from '@/screen/Home'
 import { Outlet, Route, Routes } from 'react-router'
 
@@ -14,11 +15,13 @@ const Layout = () => {
 const Navigation = () => {
   return (
     <Routes>
+      {/* Rutas con Layout */}
       <Route element={<Layout />}>
         <Route index element={<Home />} />
-
       </Route>
 
+      {/* Ruta para manejo de página no encontrada */}
+      <Route path='*' element={<Error404 />} />
     </Routes>
   )
 }
