e90765688e0ed8792858350f21f38b13a26deb0c
Raul99GC <raulc99g@gmail.com>
2024-12-01 17:36:25 -0600

    Update routing structure with additional pages and conditional footer display

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,14 +1,24 @@
+import { useLocation, Outlet, Route, Routes } from 'react-router'
+import Footer from '@/components/Footer'
 import SiteHeader from '@/components/SiteHeader'
-import Error404 from '@/screen/Error404'
-import Home from '@/screen/Home'
-import { Outlet, Route, Routes } from 'react-router'
+import Error404Page from '@/screen/Error404Page'
+import HomePage from '@/screen/HomePage'
+import LoginPage from '@/screen/LoginPage'
+import SignupPage from '@/screen/SignUp'
 
 const Layout = () => {
+  const location = useLocation()
+
+  // Verifica si la ruta actual es login o signup
+  const isLoginOrSignup = location.pathname === '/login' || location.pathname === '/signup'
+
   return (
-    <>
+    <div className='grid min-h-screen grid-rows-[auto_1fr_auto]'>
       <SiteHeader />
       <Outlet />
-    </>
+      {/* Solo muestra el Footer si no es Login o Signup */}
+      {!isLoginOrSignup && <Footer />}
+    </div>
   )
 }
 
@@ -17,11 +27,13 @@ const Navigation = () => {
     <Routes>
       {/* Rutas con Layout */}
       <Route element={<Layout />}>
-        <Route index element={<Home />} />
+        <Route path='/home' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage />} />
       </Route>
 
       {/* Ruta para manejo de página no encontrada */}
-      <Route path='*' element={<Error404 />} />
+      <Route path='*' element={<Error404Page />} />
     </Routes>
   )
 }
