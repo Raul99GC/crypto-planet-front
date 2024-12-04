0a50d479ae62e627045037f3d2995a32d673babe
Raul99GC <raulc99g@gmail.com>
2024-12-04 17:52:51 -0600

    Add ResetPasswordPage and CoinsDashBoard routes; update layout logic

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -5,19 +5,22 @@ import Error404Page from '@/screen/Error404Page'
 import HomePage from '@/screen/HomePage'
 import LoginPage from '@/screen/LoginPage'
 import SignupPage from '@/screen/SignUp'
+import ResetPasswordPage from '@/screen/ResetPasswordPage'
+import CoinsDashBoard from '@/screen/CoinsDashBoard'
 
 const Layout = () => {
   const location = useLocation()
 
   // Verifica si la ruta actual es login o signup
-  const isLoginOrSignup = location.pathname === '/login' || location.pathname === '/signup'
+  const paths = ['/login', '/signup']
+  const isAOkPath = paths.some(path => location.pathname.includes(path))
 
   return (
     <div className='grid min-h-screen grid-rows-[auto_1fr_auto]'>
       <SiteHeader />
       <Outlet />
       {/* Solo muestra el Footer si no es Login o Signup */}
-      {!isLoginOrSignup && <Footer />}
+      {!isAOkPath && <Footer />}
     </div>
   )
 }
@@ -27,9 +30,12 @@ const Navigation = () => {
     <Routes>
       {/* Rutas con Layout */}
       <Route element={<Layout />}>
+        <Route index element={<CoinsDashBoard />} />
+
         <Route path='/home' element={<HomePage />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignupPage />} />
+        <Route path='/reset-password' element={<ResetPasswordPage />} />
       </Route>
 
       {/* Ruta para manejo de página no encontrada */}
